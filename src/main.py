17528cedd019d2047dfb7c27ad587e7d401cc873
TheBarnakhil <nsakhil763@gmail.com>
2025-03-18 02:09:07 +0530

    Added static assets back into docs folder

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from path_utilites import copy_contents, generate_pages_recursive
 
 def main():
     basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
-    copy_contents('static', 'public')
+    copy_contents('static', 'docs')
     print(basepath)
     generate_pages_recursive('content', 'template.html', 'docs', basepath )
 
